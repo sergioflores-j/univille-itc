4c502ed93d85aac7ee18087d6fd22b25345669da
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2021-09-08 21:19:57 -0300

    feat: add performance metrics

--- 3_bim/caixeiro_viajante/main.py
+++ 3_bim/caixeiro_viajante/main.py
@@ -1,4 +1,6 @@
 import csv
+import time
+from datetime import timedelta
 from aleatorio import aleatorio
 from hibrido import hibrido
 from guloso import guloso
@@ -14,6 +16,13 @@ def writeResult(cidades, execucao, filename, resultRow):
 
         row = (results, resultRow[1])
         f.write(str(row))
+
+def writeTimeResult(execucao, filename, data):
+    filepath = 'results/{0}-{1}.txt'.format(execucao, filename)
+    print('writing - ', filepath)
+
+    with open(filepath, 'w', newline='', encoding='utf-8') as f:
+        f.write(str(data))
 
 def getData(rows):
     header = rows[0]
@@ -57,19 +66,27 @@ def main():
         cidadesindex.append(i)
 
     for execucao in range(execucoes):
+        aleatorio_start_time = time.time()
         res = aleatorio(matriz_distancias, cidadesindex)
         print("aleatorio - res:", res)
         writeResult(cidades, execucao, 'result-aleatorio', res)
+        writeTimeResult(execucao, 'result-aleatorio-time', time.time() - aleatorio_start_time)
 
+        guloso_start_time = time.time()
         res = guloso(matriz_distancias, cidadesindex)
         print("guloso - res:", res)
         writeResult(cidades, execucao, 'result-guloso', res)
+        writeTimeResult(execucao, 'result-guloso-time', time.time() - guloso_start_time)
 
+        hibrido_start_time = time.time()
         res = hibrido(matriz_distancias, cidadesindex)
         print("hibrido - res:", res)
         writeResult(cidades, execucao, 'result-hibrido', res)
+        writeTimeResult(execucao, 'result-hibrido-time', time.time() - hibrido_start_time)
 
 
 if __name__ == "__main__":
     print('starting')
+    program_start_time = time.time()
     main()
+    print("--- %s seconds ---" % (time.time() - program_start_time))
